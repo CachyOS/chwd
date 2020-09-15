c6f6650f14c0eb26e322990128cbae1293a408a2
Philip Müller <philm@manjaro.org>
2020-09-15 20:41:03 +0200

    [release] 0.6.5

--- src/main.cpp
+++ src/main.cpp
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
     try
     {
         Mhwd mhwd;
-        mhwd.setVersionMhwd("0.6.4","2012-2020");
+        mhwd.setVersionMhwd("0.6.5","2012-2020");
         return mhwd.launch(argc, argv);
     }
     catch(...)
