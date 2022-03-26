6fd417b5dab1dc8601c6f083f6c93be041f13dd0
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2022-03-27 02:56:14 +0400

    👷 rewrite mhwd-kernel

---
+++ scripts/kernel.cpp
@@ -0,0 +1,183 @@
+// Copyright (C) 2022 Vladislav Nepogodin
+//
+// This file is part of CachyOS kernel manager.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+#include "kernel.hpp"
+#include "ini.hpp"
+#include "utils.hpp"
+
+#include <fmt/compile.h>
+#include <fmt/core.h>
+
+#if defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wold-style-cast"
+
+#include <range/v3/algorithm/search.hpp>
+
+#pragma clang diagnostic pop
+#else
+#include <algorithm>
+#include <ranges>
+namespace ranges = std::ranges;
+#endif
+
+namespace {
+
+void parse_cachedirs(alpm_handle_t* handle) noexcept {
+    static constexpr auto cachedir = "/var/cache/pacman/pkg/";
+
+    alpm_list_t* cachedirs = nullptr;
+    cachedirs              = alpm_list_add(cachedirs, const_cast<void*>(reinterpret_cast<const void*>(cachedir)));
+    alpm_option_set_cachedirs(handle, cachedirs);
+}
+
+void parse_includes(alpm_handle_t* handle, alpm_db_t* db, const auto& section, const auto& file) noexcept {
+    const auto* archs = alpm_option_get_architectures(handle);
+    const auto* arch  = reinterpret_cast<const char*>(archs->data);
+
+    mINI::INIFile file_nested(file);
+    // next, create a structure that will hold data
+    mINI::INIStructure mirrorlist;
+
+    // now we can read the file
+    file_nested.read(mirrorlist);
+    for (const auto& mirror : mirrorlist) {
+        auto repo = mirror.second.begin()->second;
+        if (repo.starts_with("/")) {
+            continue;
+        }
+        utils::replace_all(repo, "$arch", arch);
+        utils::replace_all(repo, "$repo", section.c_str());
+        alpm_db_add_server(db, repo.c_str());
+    }
+}
+
+void parse_repos(alpm_handle_t* handle) noexcept {
+    static constexpr auto pacman_conf_path = "/etc/pacman.conf";
+    static constexpr auto ignored_repo     = "testing";
+
+    mINI::INIFile file(pacman_conf_path);
+    // next, create a structure that will hold data
+    mINI::INIStructure ini;
+
+    // now we can read the file
+    file.read(ini);
+    for (const auto& it : ini) {
+        const auto& section = it.first;
+        const auto& nested  = it.second;
+        if (section == ignored_repo) {
+            continue;
+        }
+        if (section == "options") {
+            for (const auto& it_nested : nested) {
+                if (it_nested.first != "architecture") {
+                    continue;
+                }
+                // add CacheDir
+                const auto& archs = utils::make_multiline(it_nested.second, false, " ");
+                for (const auto& arch : archs) {
+                    alpm_option_add_architecture(handle, arch.c_str());
+                }
+            }
+            continue;
+        }
+        auto* db = alpm_register_syncdb(handle, section.c_str(), ALPM_SIG_USE_DEFAULT);
+
+        for (const auto& it_nested : nested) {
+            const auto& param = it_nested.first;
+            const auto& value = it_nested.second;
+            if (param == "include") {
+                parse_includes(handle, db, section, value);
+            }
+        }
+    }
+}
+}  // namespace
+
+std::string Kernel::version() const noexcept {
+    const char* sync_pkg_ver = alpm_pkg_get_version(m_pkg);
+    if (!is_installed()) {
+        return sync_pkg_ver;
+    }
+
+    auto* db        = alpm_get_localdb(m_handle);
+    auto* local_pkg = alpm_db_get_pkg(db, m_name.c_str());
+    return alpm_pkg_get_version(local_pkg);
+}
+
+// Name must be without any repo name (e.g. core/linux)
+bool Kernel::is_installed() const noexcept {
+    auto* db  = alpm_get_localdb(m_handle);
+    auto* pkg = alpm_db_get_pkg(db, m_name.c_str());
+
+    return pkg != NULL;
+}
+
+// Find kernel packages by finding packages which have words 'linux' and 'headers'.
+// From the output of 'pacman -Sl'
+// - find lines that have words: 'linux' and 'headers'
+// - drop lines containing 'testing' (=testing repo, causes duplicates) and 'linux-api-headers' (=not a kernel header)
+// - show the (header) package names
+// Now we have names of the kernel headers.
+// Then add the kernel packages to proper places and output the result.
+// Then display possible kernels and headers added by the user.
+
+// The output consists of a list of reponame and a package name formatted as: "reponame/pkgname"
+// For example:
+//    reponame/linux-xxx reponame/linux-xxx-headers
+//    reponame/linux-yyy reponame/linux-yyy-headers
+//    ...
+std::vector<Kernel> Kernel::get_kernels(alpm_handle_t* handle) noexcept {
+    static constexpr std::string_view ignored_pkg  = "linux-api-headers";
+    static constexpr std::string_view replace_part = "-headers";
+    std::vector<Kernel> kernels{};
+
+    parse_repos(handle);
+    parse_cachedirs(handle);
+
+    auto* dbs = alpm_get_syncdbs(handle);
+    for (alpm_list_t* i = dbs; i != nullptr; i = i->next) {
+        static constexpr auto needle = "linux[^ ]*-headers";
+        alpm_list_t* needles         = nullptr;
+        alpm_list_t* ret_list        = nullptr;
+        needles                      = alpm_list_add(needles, const_cast<void*>(reinterpret_cast<const void*>(needle)));
+
+        auto* db            = reinterpret_cast<alpm_db_t*>(i->data);
+        const char* db_name = alpm_db_get_name(db);
+        alpm_db_search(db, needles, &ret_list);
+
+        for (alpm_list_t* j = ret_list; j != nullptr; j = j->next) {
+            auto* pkg            = reinterpret_cast<alpm_pkg_t*>(j->data);
+            std::string pkg_name = alpm_pkg_get_name(pkg);
+
+            const auto& found = ranges::search(pkg_name, ignored_pkg);
+            if (!found.empty()) {
+                continue;
+            }
+            utils::remove_all(pkg_name, replace_part);
+            pkg = alpm_db_get_pkg(db, pkg_name.c_str());
+
+            kernels.emplace_back(Kernel{handle, pkg, db_name, fmt::format(FMT_COMPILE("{}/{}"), db_name, pkg_name)});
+        }
+
+        alpm_list_free(needles);
+        alpm_list_free(ret_list);
+    }
+
+    return kernels;
+}
---
+++ scripts/mhwd-kernel.cpp
@@ -0,0 +1,231 @@
+// Copyright (C) 2022 Vladislav Nepogodin
+//
+// This file is part of CachyOS Mhwd.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+#include "kernel.hpp"
+
+#include <unistd.h>
+
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <string_view>
+
+#if defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wold-style-cast"
+
+#include <range/v3/algorithm/any_of.hpp>
+#include <range/v3/algorithm/none_of.hpp>
+
+#pragma clang diagnostic pop
+#else
+#include <algorithm>
+#include <ranges>
+namespace ranges = std::ranges;
+#endif
+
+#include <fmt/core.h>
+
+namespace {
+class AlpmHelper {
+ public:
+    AlpmHelper() = default;
+    ~AlpmHelper() {
+        alpm_release(m_handle);
+    }
+
+    [[nodiscard]] const std::vector<Kernel>& kernels() const noexcept {
+        return m_kernels;
+    }
+
+ private:
+    alpm_errno_t m_err{};
+    alpm_handle_t* m_handle       = alpm_initialize("/", "/var/lib/pacman/", &m_err);
+    std::vector<Kernel> m_kernels = Kernel::get_kernels(m_handle);
+};
+
+void err(const std::string_view& msg) noexcept {
+    fmt::print(stderr, "\033[31mError:\033[0m {}\n", msg);
+    std::exit(1);
+}
+
+void root_check() noexcept {
+    if (geteuid() == 0) return;
+    err("Please run as root.");
+}
+
+void kernel_usage() noexcept {
+    std::cout <<
+        R"(Usage: mhwd-kernel [option]
+    -h  --help              Show this help message
+    -i  --install           Install a new kernel        [kernel(s)] [optional: rmc = remove current kernel]
+    -l  --list              List all available kernels
+    -li --listinstalled     List installed kernels
+    -lr --listrunning       List running kernel
+    -r  --remove            Remove a kernel             [kernel(s)])"
+              << '\n';
+}
+
+std::string exec(const std::string_view& command) noexcept {
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(command.data(), "r"), pclose);
+    if (!pipe) {
+        return "-1";
+    }
+
+    std::string result{};
+    std::array<char, 128> buffer{};
+    while (!feof(pipe.get())) {
+        if (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+            result += buffer.data();
+        }
+    }
+
+    if (result.ends_with('\n')) {
+        result.pop_back();
+    }
+
+    return result;
+}
+
+std::string kernel_running() noexcept {
+    return exec(R"(grep -Po '(?<=initrd\=\\initramfs-)(.+)(?=\.img)|(?<=boot\/vmlinuz-)([^ $]+)' /proc/cmdline)");
+}
+const auto current = kernel_running();
+const auto helper  = AlpmHelper();
+
+void kernel_repo(FILE* fd = stdout) noexcept {
+    const auto& kernels = helper.kernels();
+
+    fmt::print(stderr, "\033[32mavailable kernels:\033[0m\n");
+    for (const auto& kernel : kernels) {
+        fmt::print(fd, "{} {}\n", kernel.get_raw(), kernel.version());
+    }
+}
+
+void kernel_list(FILE* fd = stdout) noexcept {
+    const auto& kernels = helper.kernels();
+
+    fmt::print(stderr, "\033[32mCurrently running:\033[0m {} ({})\n", exec("uname -r"), current);
+    fmt::print(fd, "The following kernels are installed in your system:\n");
+    for (const auto& kernel : kernels) {
+        if (!kernel.is_installed()) { continue; }
+        fmt::print(fd, "local/{} {}\n", kernel.name(), kernel.version());
+    }
+}
+
+void kernel_install(const std::vector<std::string>& kernels) noexcept {
+    std::string pkginstall{};
+    bool rmc{};
+
+    for (const auto& kernel : kernels) {
+        if (kernel == "rmc") { rmc = true; continue; }
+        else if (current == kernel) {
+            err("You can't reinstall your current kernel. Please use 'pacman -Syu' instead to update.");
+        } else if (ranges::none_of(helper.kernels(), [&](auto&& elem) {
+                       return elem.name() == kernel;
+                   })) {
+            fmt::print(stderr, "\033[31mError:\033[0m Please make sure if the given kernel(s) exist(s).\n");
+            kernel_repo(stderr);
+            std::exit(1);
+        }
+
+        pkginstall += fmt::format("{} ", kernel);
+    }
+    std::system("pacman -Syy");
+
+    const auto& outofdate = exec("pacman -Qqu | tr '\n' ' '");
+    if (!outofdate.empty()) {
+        fmt::print(stderr, "The following packages are out of date, please update your system first: {}\n", outofdate);
+        fmt::print("Do you want to continue anyway? [y/N] ");
+        char yesno{'N'};
+        std::cin >> yesno;
+        fmt::print("\n");
+        if (yesno != 'Y' && yesno != 'y') { std::exit(1); }
+    }
+
+    auto cmd              = fmt::format("pacman -Syu \"{}\"", pkginstall);
+    const int status_code = std::system(cmd.c_str());
+
+    cmd = fmt::format("pacman -R \"{}\"", current);
+    if (rmc && (status_code == 0)) {
+        std::system(cmd.c_str());
+    } else if (rmc && (status_code != 0)) {
+        err("\n'rmc' aborted because the kernel failed to install or canceled on removal.");
+    }
+}
+
+void kernel_remove(const std::vector<std::string>& kernels) noexcept {
+    std::string pkgremove{};
+
+    for (const auto& kernel : kernels) {
+        if (kernel.empty()) { err("Invalid argument (use -h for help)."); }
+        else if (current == kernel) { err("You can't remove your current kernel."); }
+        else if (!ranges::any_of(helper.kernels(), [&](auto&& elem) {
+                       return elem.is_installed() && (elem.name() == kernel);
+                   })) {
+            fmt::print(stderr, "\033[31mError:\033[0m Kernel not installed.\n");
+            kernel_list(stderr);
+            std::exit(1);
+        }
+
+        pkgremove += fmt::format("{} ", kernel);
+    }
+
+    const auto& cmd = fmt::format("pacman -R \"{}\"", pkgremove);
+    std::system(cmd.c_str());
+}
+
+}  // namespace
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        err("No arguments given (use -h for help).");
+    }
+
+    const auto& process_args = [argc, argv]() {
+        std::vector<std::string> kernels{};
+        kernels.reserve(static_cast<size_t>(argc - 1));
+        for (int i = 2; i < argc; ++i) {
+            kernels.emplace_back(argv[i]);
+        }
+        return kernels;
+    };
+    const std::string_view argument = argv[1];
+    if (argument == "-h" || argument == "--help") {
+        kernel_usage();
+        return 0;
+    } else if (argument == "-i" || argument == "--install") {
+        root_check();
+        kernel_install(process_args());
+        return 0;
+    } else if (argument == "-l" || argument == "--list") {
+        kernel_repo();
+        return 0;
+    } else if (argument == "-li" || argument == "--listinstalled") {
+        kernel_list();
+        return 0;
+    } else if (argument == "-lr" || argument == "--listrunning") {
+        fmt::print("{}\n", current);
+        return 0;
+    } else if (argument == "-r" || argument == "--remove") {
+        root_check();
+        kernel_remove(process_args());
+        return 0;
+    }
+    err("Invalid argument (use -h for help).");
+}
