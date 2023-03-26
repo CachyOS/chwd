540130a469085db79944cde9b45a131652f969de
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2023-03-26 04:57:30 +0400

    👷 replace mhwd-kernel with chwd-kernel

---
+++ scripts/chwd-kernel/src/kernel.rs
@@ -0,0 +1,107 @@
+// Copyright (C) 2022-2023 Vladislav Nepogodin
+//
+// This file is part of CachyOS kernel manager.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+use std::sync::Arc;
+
+const IGNORED_PKG: &str = "linux-api-headers";
+const REPLACE_PART: &str = "-headers";
+//const NEEDLE: &str = "linux[^ ]*-headers";
+
+#[derive(Debug)]
+pub struct Kernel<'a> {
+    pub name: String,
+    pub repo: String,
+    pub raw: String,
+    alpm_pkg: Option<alpm::Package<'a>>,
+    alpm_handle: Option<&'a alpm::Alpm>,
+}
+
+impl Kernel<'_> {
+    pub fn new() -> Self {
+        Self {
+            name: "".to_owned(),
+            repo: "local".to_owned(),
+            raw: "".to_owned(),
+            alpm_pkg: None,
+            alpm_handle: None,
+        }
+    }
+
+    // Name must be without any repo name (e.g. core/linux)
+    pub fn is_installed(&self) -> Option<bool> {
+        let local_db = self.alpm_handle.as_ref()?.localdb();
+        Some(local_db.pkg(self.name.as_bytes()).is_ok())
+    }
+
+    pub fn version(&self) -> Option<String> {
+        if !self.is_installed().unwrap() {
+            return Some(self.alpm_pkg.as_ref()?.version().to_string());
+        }
+
+        let local_db = self.alpm_handle.as_ref()?.localdb();
+        let local_pkg = local_db.pkg(self.name.as_bytes());
+
+        Some(local_pkg.ok()?.version().to_string())
+    }
+}
+
+/// Find kernel packages by finding packages which have words 'linux' and 'headers'.
+/// From the output of 'pacman -Sl'
+/// - find lines that have words: 'linux' and 'headers'
+/// - drop lines containing 'testing' (=testing repo, causes duplicates) and 'linux-api-headers' (=not a kernel header)
+/// - show the (header) package names
+/// Now we have names of the kernel headers.
+/// Then add the kernel packages to proper places and output the result.
+/// Then display possible kernels and headers added by the user.
+
+/// The output consists of a list of reponame and a package name formatted as: "reponame/pkgname"
+/// For example:
+///    reponame/linux-xxx reponame/linux-xxx-headers
+///    reponame/linux-yyy reponame/linux-yyy-headers
+///    ...
+pub fn get_kernels(alpm_handle: &alpm::Alpm) -> Vec<Kernel> {
+    let mut kernels = Vec::new();
+    let needles: &[String] = &["linux-[a-z]".into(), "headers".into()];
+    //let needles: &[String] = &["linux[^ ]*-headers".into()];
+
+    for db in alpm_handle.syncdbs() {
+        let db_name = db.name();
+        // search each database for packages matching the regex "linux-[a-z]" AND "headers"
+        for pkg_headers in db.search(needles.iter()).unwrap() {
+            let mut pkg_name = pkg_headers.name().to_owned();
+            if pkg_name.contains(IGNORED_PKG) {
+                continue;
+            }
+            pkg_name = pkg_name.replace(REPLACE_PART, "");
+
+            // Skip if the actual kernel package is not found
+            if let Ok(pkg) = db.pkg(pkg_name.as_bytes()) {
+                kernels.push(Kernel {
+                    name: pkg_name.clone(),
+                    repo: db_name.to_string(),
+                    raw: format!("{}/{}", db_name, pkg_name),
+
+                    alpm_pkg: Some(pkg),
+                    alpm_handle: Some(alpm_handle),
+                });
+            }
+        }
+    }
+
+    kernels
+}
---
+++ scripts/chwd-kernel/src/main.rs
@@ -0,0 +1,245 @@
+// Copyright (C) 2022-2023 Vladislav Nepogodin
+//
+// This file is part of CachyOS chwd.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+mod kernel;
+
+use clap::Parser;
+use dialoguer::Confirm;
+use once_cell::sync::Lazy;
+use std::sync::{Arc, Mutex};
+use subprocess::{Exec, Redirection};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+#[command(arg_required_else_help(true))]
+struct Args {
+    /// List available kernels
+    #[arg(long)]
+    list: bool,
+
+    /// List installed kernels
+    #[arg(long)]
+    list_installed: bool,
+
+    /// Show running kernel
+    #[arg(long)]
+    running_kernel: bool,
+
+    /// Install kernels
+    #[arg(short, long = "install", conflicts_with("remove_kernels"))]
+    install_kernels: Vec<String>,
+
+    /// Uninstall kernels
+    #[arg(short, long = "remove", conflicts_with("install_kernels"))]
+    remove_kernels: Vec<String>,
+}
+
+enum WorkingMode {
+    KernelInstall,
+    KernelRemove,
+}
+
+fn new_alpm() -> alpm::Result<alpm::Alpm> {
+    let pacman = pacmanconf::Config::with_opts(None, Some("/etc/pacman.conf"), Some("/")).unwrap();
+    let alpm = alpm_utils::alpm_with_conf(&pacman)?;
+
+    Ok(alpm)
+}
+
+fn simple_shell_exec(cmd: &str) -> String {
+    let mut exec_out = Exec::shell(cmd)
+        .stdout(Redirection::Pipe)
+        .capture()
+        .unwrap()
+        .stdout_str();
+    exec_out.pop();
+    exec_out
+}
+
+#[inline]
+fn get_kernel_running() -> String {
+    simple_shell_exec(
+        r#"(grep -Po '(?<=initrd\=\\initramfs-)(.+)(?=\.img)|(?<=boot\/vmlinuz-)([^ $]+)' /proc/cmdline)"#,
+    )
+}
+
+fn root_check() {
+    if nix::unistd::geteuid().is_root() {
+        return;
+    }
+    occur_err("Please run as root.");
+}
+
+fn occur_err(msg: &str) {
+    eprintln!("\x1B[31mError:\x1B[0m {}", msg);
+    std::process::exit(1);
+}
+
+fn show_installed_kernels(kernels: &[kernel::Kernel]) {
+    let current_kernel = get_kernel_running();
+    println!(
+        "\x1B[32mCurrently running:\x1B[0m {} ({})",
+        simple_shell_exec("uname -r"),
+        current_kernel
+    );
+    println!("The following kernels are installed in your system:");
+    for kernel in kernels {
+        if !kernel.is_installed().unwrap() {
+            continue;
+        }
+        println!("local/{} {}", kernel.name, kernel.version().unwrap());
+    }
+}
+
+fn show_available_kernels(kernels: &[kernel::Kernel]) {
+    println!("\x1B[32mavailable kernels:\x1B[0m",);
+    for kernel in kernels {
+        println!("{} {}", kernel.raw, kernel.version().unwrap());
+    }
+}
+
+fn kernel_install(available_kernels: &[kernel::Kernel], kernel_names: &[String]) -> bool {
+    let mut pkginstall = String::new();
+    let mut rmc = false;
+
+    let current_kernel = get_kernel_running();
+    for kernel_name in kernel_names {
+        if kernel_name == "rmc" {
+            rmc = true;
+            continue;
+        } else if &current_kernel == kernel_name {
+            occur_err("You can't reinstall your current kernel. Please use 'pacman -Syu' instead to update.");
+        } else if available_kernels
+            .iter()
+            .all(|elem| &elem.name != kernel_name)
+        {
+            eprintln!("\x1B[31mError:\x1B[0m Please make sure if the given kernel(s) exist(s).");
+            show_available_kernels(available_kernels);
+            return false;
+        }
+
+        pkginstall.push_str(&format!("{} ", &kernel_name));
+    }
+    let _ = Exec::shell("pacman -Syy").join();
+
+    let outofdate = simple_shell_exec("pacman -Qqu | tr '\n' ' '");
+    if !outofdate.is_empty() {
+        eprintln!(
+            "The following packages are out of date, please update your system first: {}",
+            outofdate
+        );
+        if !Confirm::new()
+            .with_prompt("Do you want to continue anyway?")
+            .default(true)
+            .interact()
+            .unwrap()
+        {
+            return false;
+        }
+    }
+
+    let exit_status = Exec::shell(format!("pacman -Syu {}", pkginstall))
+        .join()
+        .unwrap();
+    if rmc && exit_status.success() {
+        let _ = Exec::shell(format!("pacman -R {}", current_kernel)).join();
+    } else if rmc && !exit_status.success() {
+        occur_err("\n'rmc' aborted because the kernel failed to install or canceled on removal.");
+    }
+    true
+}
+
+fn kernel_remove(available_kernels: &[kernel::Kernel], kernel_names: &[String]) -> bool {
+    let mut pkgremove = String::new();
+
+    let current_kernel = get_kernel_running();
+    for kernel_name in kernel_names {
+        if &current_kernel == kernel_name {
+            occur_err("You can't remove your current kernel.");
+        } else if !available_kernels
+            .iter()
+            .any(|elem| elem.is_installed().unwrap() && (&elem.name == kernel_name))
+        {
+            eprintln!("\x1B[31mError:\x1B[0m Kernel is not installed.");
+            show_installed_kernels(available_kernels);
+            return false;
+        }
+
+        pkgremove.push_str(&format!("{} ", kernel_name));
+    }
+
+    let exit_status = Exec::shell(format!("pacman -R {}", pkgremove))
+        .join()
+        .unwrap();
+    exit_status.success()
+}
+
+static ALPM_HANDLE: Lazy<Arc<Mutex<alpm::Alpm>>> = Lazy::new(|| {
+    let alpm_handle = new_alpm().expect("Unable to initialize Alpm");
+
+    Arc::new(Mutex::new(alpm_handle))
+});
+
+fn main() {
+    let args = Args::parse();
+
+    if args.list {
+        let alpm_handle = &*ALPM_HANDLE.lock().unwrap();
+        let kernels = kernel::get_kernels(&alpm_handle);
+        show_available_kernels(&kernels);
+        return;
+    }
+
+    if args.list_installed {
+        let alpm_handle = &*ALPM_HANDLE.lock().unwrap();
+        let kernels = kernel::get_kernels(&alpm_handle);
+        show_installed_kernels(&kernels);
+        return;
+    }
+
+    if args.running_kernel {
+        println!("\x1B[32mrunning kernel:\x1B[0m '{}'", get_kernel_running());
+        return;
+    }
+
+    let working_mode = if !args.install_kernels.is_empty() {
+        Some(WorkingMode::KernelInstall)
+    } else if !args.remove_kernels.is_empty() {
+        Some(WorkingMode::KernelRemove)
+    } else {
+        None
+    };
+
+    match working_mode {
+        Some(WorkingMode::KernelInstall) => {
+            root_check();
+
+            let alpm_handle = &*ALPM_HANDLE.lock().unwrap();
+            let kernels = kernel::get_kernels(&alpm_handle);
+            kernel_install(&kernels, &args.install_kernels);
+        }
+        Some(WorkingMode::KernelRemove) => {
+            root_check();
+
+            let alpm_handle = &*ALPM_HANDLE.lock().unwrap();
+            let kernels = kernel::get_kernels(&alpm_handle);
+            kernel_remove(&kernels, &args.remove_kernels);
+        }
+        _ => occur_err("Invalid argument (use -h for help)."),
+    };
+}
