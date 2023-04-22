566c663a3050fd8f965db9ec8e1dfc139e1c19cd
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2023-04-22 17:46:58 +0400

    🚧 backend: fix compilation

--- chwd-rust/src/lib.rs
+++ chwd-rust/src/lib.rs
@@ -14,8 +14,6 @@
 // with this program; if not, write to the Free Software Foundation, Inc.,
 // 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-#![feature(extern_types)]
-
 pub mod console_writer;
 pub mod consts;
 pub mod data;
