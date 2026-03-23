CachyOS/chwd

commit 7c3043696a68b8ccee4ef005e48d2982f8fae8d4

👷 support product name pattern match

src/data.rs
@@ -210,10 +210,23 @@ fn set_matching_profiles(
 pub fn get_all_devices_of_profile(devices: &ListOfDevicesT, profile: &Profile) -> Vec<usize> {
     let mut found_indices = vec![];
 
-    let re: Option<Regex> = profile
+    let dev_name_re: Option<Regex> = profile
         .device_name_pattern
         .as_ref()
         .map(|dev_pattern| Regex::new(dev_pattern).expect("Failed to initialize regex"));
+
+    let product_name_re: Option<Regex> = profile
+        .hwd_product_name_pattern
+        .as_ref()
+        .map(|product_pattern| Regex::new(product_pattern).expect("Failed to initialize regex"));
+
+    if let Some(product_name_re) = &product_name_re {
+        let product_name = fs::read_to_string("/sys/devices/virtual/dmi/id/product_name")
+            .expect("Failed to read product name");
+        if !product_name_re.is_match(&product_name) {
+            return vec![];
+        }
+    }
 
     for hwd_id in profile.hwd_ids.iter() {
         let mut found_device = false;
@@ -246,8 +259,8 @@ pub fn get_all_devices_of_profile(devices: &ListOfDevicesT, profile: &Profile) -
                             .any(|x| x.to_lowercase() == i_device.vendor_id.to_lowercase());
                         if !found {
                             // Check device ids
-                            found = if let Some(re) = &re {
-                                re.is_match(&i_device.device_name)
+                            found = if let Some(dev_name_re) = &dev_name_re {
+                                dev_name_re.is_match(&i_device.device_name)
                             } else {
                                 hwd_id.device_ids.iter().any(|x| {
                                     x == "*"

graphic_drivers-profiles-test.toml
@@ -68,3 +68,4 @@ post_remove = """
     mkinitcpio -P
 """
 device_ids = '>nvidia-test-ids.ids'
+hwd_product_name_pattern = '(Ally)\w+'

profile-raw-escaped-strings-test.toml
@@ -4,6 +4,7 @@ class_ids = "0300"
 desc = "Test profile"
 device_ids = "1435 163f"
 device_name_pattern = '(AD)\w+'
+hwd_product_name_pattern = '(Ally)\w+'
 nonfree = false
 packages = "opencl-rusticl-mesa lib32-opencl-rusticl-mesa rocm-opencl-runtime"
 post_install = """

src/profile.rs
@@ -45,6 +45,7 @@ pub struct Profile {
     pub post_install: String,
     pub post_remove: String,
     pub device_name_pattern: Option<String>,
+    pub hwd_product_name_pattern: Option<String>,
 
     pub hwd_ids: Vec<HardwareID>,
 }
@@ -150,6 +151,9 @@ fn parse_profile(node: &toml::Table, profile_name: &str) -> Result<Profile> {
         device_name_pattern: node
             .get("device_name_pattern")
             .and_then(|x| x.as_str().map(str::to_string)),
+        hwd_product_name_pattern: node
+            .get("hwd_product_name_pattern")
+            .and_then(|x| x.as_str().map(str::to_string)),
     };
 
     let conf_devids = node.get("device_ids").and_then(|x| x.as_str()).unwrap_or("");
@@ -246,6 +250,9 @@ pub fn write_profile_to_file(file_path: &str, profile: &Profile) -> bool {
     if let Some(dev_name_pattern) = &profile.device_name_pattern {
         table.insert("device_name_pattern".to_owned(), dev_name_pattern.clone().into());
     }
+    if let Some(product_name_pattern) = &profile.hwd_product_name_pattern {
+        table.insert("hwd_product_name_pattern".to_owned(), product_name_pattern.clone().into());
+    }
 
     let last_hwd_id = profile.hwd_ids.last().unwrap();
 
@@ -340,6 +347,7 @@ mod tests {
              lib32-nvidia-utils libva-nvidia-driver vulkan-icd-loader lib32-vulkan-icd-loader"
         );
         assert_eq!(parsed_profiles[1].device_name_pattern, None);
+        assert_eq!(parsed_profiles[1].hwd_product_name_pattern, Some("(Ally)\\w+".to_owned()));
         assert_eq!(parsed_profiles[1].hwd_ids, hwd_ids);
         assert!(!parsed_profiles[1].post_install.is_empty());
         assert!(!parsed_profiles[1].post_remove.is_empty());

commit f794748df24dba071d8c2e3152c670d9f9f8b4a1

data: Trim newlines from product_name
fs::read_to_string(path) returns `product_name\n`. This breaks pattern
matching with string anchors fail as the pattern fails to match against
the newline. Trim newlines from product_name so that matching patterns
with string anchors work correctly.

Fixes: 7c3043696a68 ("👷 support product name pattern match")
Signed-off-by: Eric Naim <dnaim@cachyos.org>

src/data.rs
@@ -242,7 +242,8 @@ pub fn get_all_devices_of_profile(devices: &ListOfDevicesT, profile: &Profile) -
     if let Some(product_name_re) = &product_name_re {
         let product_name = fs::read_to_string("/sys/devices/virtual/dmi/id/product_name")
             .expect("Failed to read product name");
-        if !product_name_re.is_match(&product_name) {
+        let product_name = product_name.trim();
+        if !product_name_re.is_match(product_name) {
             return vec![];
         }
     }